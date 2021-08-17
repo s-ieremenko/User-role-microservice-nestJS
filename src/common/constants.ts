1677e33038374f11a05af9d285de7599dca99dea
s-ieremenko <ieremenko.sveta@gmail.com>
2021-08-17 17:12:06 +0300

    moved constants to the separate file

---
+++ src/common/constants.ts
@@ -0,0 +1,6 @@
+export const typeDB = 'postgres';
+export const hostDB = 'localhost';
+export const portDB = 5432;
+export const dialect = 'postgres';
+export const passwordDB = '12345678';
+export const databaseName = 'users1';
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,5 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+  databaseName,
+  dialect,
+  hostDB,
+  passwordDB,
+  portDB,
+  typeDB,
+} from 'src/common/constants';
 import { Permission } from 'src/permission/permission.entity';
 import { Role } from 'src/role/role.entity';
 import { User } from 'src/user/user.entity';
@@ -7,12 +15,12 @@ import { User } from 'src/user/user.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '12345678',
-      database: 'users1',
+      type: typeDB,
+      host: hostDB,
+      port: portDB,
+      username: dialect,
+      password: passwordDB,
+      database: databaseName,
       entities: [Role, User, Permission],
       synchronize: true,
     }),
