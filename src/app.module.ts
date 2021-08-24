9fd5f0fd9e80f0c16aa81270486fc24f995e722d
s-ieremenko <ieremenko.sveta@gmail.com>
2021-08-24 21:47:54 +0300

    added .env, created typeormconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfigAsync } from './config/typeorm.config';
 
 import { UserModule } from './user/user.module';
 import { RoleModule } from './role/role.module';
 import { PermissionModule } from './permission/permission.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, RoleModule, PermissionModule],
+  imports: [
+    UserModule,
+    RoleModule,
+    PermissionModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,31 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+import { Permission } from 'src/permission/permission.entity';
+import { Role } from 'src/role/role.entity';
+import { User } from 'src/user/user.entity';
+
+export default class TypeOrmConfig {
+  static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: configService.get('DB_HOST'),
+      port: configService.get('DB_PORT'),
+      username: configService.get('DB_USERNAME'),
+      password: configService.get('DB_PASSWORD'),
+      database: configService.get('DB_NAME'),
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: true,
+    };
+  }
+}
+
+export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => TypeOrmConfig.getOrmConfig(configService),
+  inject: [ConfigService],
+};
